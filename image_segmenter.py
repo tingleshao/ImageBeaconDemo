400cb9fd84982bba1a2bcb90e79564be0c621c6e
tingleshao <cshao@cs.unc.edu>
2017-02-22 22:50:14 -0500

    add image processing code template; add gitignore.

---
+++ image_segmenter.py
@@ -0,0 +1,15 @@
+
+
+class image_segmenter():
+    def set_images(image1, image2):
+        self.image1 = image1
+        self.image2 = image2
+
+    def segment():
+        return None
+
+    def watershed():
+        return None
+
+    def disparity():
+        return None
