1adf8ac4743d2244c99c6252478011fd0140a1df
tingleshao <cshao@cs.unc.edu>
2017-02-26 14:31:19 -0500

    update image encoder

--- image_encoder.py
+++ image_encoder.py
@@ -1,7 +1,7 @@
-
-
 # this class basically, transforms the image into 64 and 64 and with DCT and,
 # later add a segmentation map into consideration\
+
+import cv2
 
 
 class image_encoder():
@@ -9,7 +9,14 @@ class image_encoder():
         print "Image encoder initialized!"
 
     def encode_image(img):
-        return None
+        # downsample the image into 64x64
+        img_small = cv2.resize(img, (64,64), interpolation=cv2.INTER_CUBIC)
+        # dct the image
+        imf = np.float32(img_small)/255.0  # float conversion/scale
+        dst = cv2.dct(imf)           # the dct
+        # TODO: add statistical compression on it.
+        return dst
+    #    imgcv1 = np.uint8(dst)*255.0    # convert back
 
     def encode_image_with_segmentation(img, mask):
-        return None 
+        return None
