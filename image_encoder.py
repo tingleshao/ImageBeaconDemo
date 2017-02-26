d3a8c67a47af07317d77eced8ddfc16374946d73
tingleshao <cshao@cs.unc.edu>
2017-02-26 14:04:39 -0500

    add document in image encoder

--- image_encoder.py
+++ image_encoder.py
@@ -1,8 +1,15 @@
 
 
+# this class basically, transforms the image into 64 and 64 and with DCT and,
+# later add a segmentation map into consideration\
+
+
 class image_encoder():
     def init():
+        print "Image encoder initialized!"
+
+    def encode_image(img):
         return None
 
-    def encode_image():
-        return None
+    def encode_image_with_segmentation(img, mask):
+        return None 
