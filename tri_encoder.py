f8aa55ce1ed84c49b2524cd9adf5ab69997d121b
tingleshao <cshao@cs.unc.edu>
2017-04-06 13:23:19 -0400

    save upadte

--- tri_encoder.py
+++ tri_encoder.py
@@ -2,3 +2,16 @@
 
 import cv2
 import numpy as np
+
+
+class tri_encoder():
+    def init():
+        print("Triangle encoer initialized!")
+
+    def encode(self, img, processed):
+        # downsample image into 64 x 64
+        if not processed:
+            img = img[:, 324:2267]
+        img_small = cv2.resize(img, (64, 64), interpolation=cv2.INTER_CUBIC)
+        # triangularization the image
+        
